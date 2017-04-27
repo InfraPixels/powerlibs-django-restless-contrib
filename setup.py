5843b542d8fdb58d97dc12aba3bb76e78bd76951
cleber <cleber@dronemapp.com>
2017-04-27 12:17:26 -0300

    add powerlibs.django.restless to requirements and updates setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+import re
+
 try:
     from setuptools import setup
 except ImportError:
@@ -5,8 +7,23 @@ except ImportError:
 
 version = '0.6.0'
 
+
+def pip_git_to_setuptools_git(url):
+    match = re.match(r'git\+https://github.com/(?P<organization>[^/]+)/(?P<repository>[^/]+).git@(?P<tag>.+)', url.strip())
+    if match:
+        url = 'http://github.com/{organization}/{repository}/tarball/master#egg={tag}'.format(
+            **match.groupdict()
+        )
+    return url
+
+requires = []
+dependency_links = []
 with open('requirements/production.txt') as requirements_file:
-    requires = [line for line in requirements_file]
+    for line in requirements_file:
+        if 'git+http' in line:
+            dependency_links.append(pip_git_to_setuptools_git(line))
+        else:
+            requires.append(line)
 
 with open('README.md') as f:
     readme = f.read()
@@ -23,10 +40,11 @@ setup(
     author_email='cleberman@gmail.com',
     url='https://github.com/Dronemapp/powerlibs-django-restless-contrib',
     license=license,
-    packages=['powerlibs.django.restless.contrib', 'powerlibs.django.restless.contrib.endpoints'],
+    packages=['powerlibs.django.restless.contrib.endpoints'],
     package_data={'': ['LICENSE', 'README.md']},
     include_package_data=True,
     install_requires=requires,
+    dependency_links=dependency_links,
     zip_safe=False,
     keywords='generic libraries',
     classifiers=(
