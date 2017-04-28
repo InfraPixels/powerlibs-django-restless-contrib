a37e34c4abca771eb147a8a41f1fd0714479d992
cleber <cleber@dronemapp.com>
2017-04-28 11:40:36 -0300

    improve test coverage

--- tests/test_nested_endpoints.py
+++ tests/test_nested_endpoints.py
@@ -7,3 +7,11 @@ def test_nested_detail_endpoint_mixin(nested_detail_endpoint):
     response = nested_detail_endpoint.get(mocked_request)
 
     assert '_related' in response
+
+
+def test_nested_list_endpoint_mixin(nested_list_endpoint):
+    mocked_request = mock.Mock(GET={'_nested': 'related_model'})
+
+    response = nested_list_endpoint.get(mocked_request)
+
+    assert '_related' in response[0]
