038c66617c66702e31bf1f24e9e1072c4c89a98a
cleber <cleber@dronemapp.com>
2018-11-07 10:27:37 -0200

    bugfix on bbox

--- powerlibs/django/restless/contrib/endpoints/bbox.py
+++ powerlibs/django/restless/contrib/endpoints/bbox.py
@@ -3,14 +3,24 @@ from shapely.geometry import box
 
 class BoundingBoxListEndpointMixin:
     def get_query_set(self, request, *args, **kwargs):
-        queryset = super().get_query_set(request, *args, **kwargs)
+        used_keys = []
+        queryset_args = {}
 
         for key, value in request.GET.items():
             if key.endswith('__bbox'):
                 field_name = key.replace('__bbox', '')
                 bbox_coords = tuple(float(x) for x in value.split(','))
-                bbox = box(bbox_coords)
+                bbox = box(*bbox_coords)
                 field_arg_name = f'{field_name}__within'
-                queryset = queryset.filter(**{field_arg_name: bbox.to_wkt()})
+                queryset_args[field_arg_name] = bbox.to_wkt()
+                used_keys.append(key)
+
+        if used_keys:
+            request.GET._mutable = True
+            for key in used_keys:
+                del request.GET[key]
+
+        queryset = super().get_query_set(request, *args, **kwargs)
+        queryset = queryset.filter(**queryset_args)
 
         return queryset
